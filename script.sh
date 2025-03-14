0340dcdc0441457630fe39c6502377ea881bc498
Salvatore <dipietro.salvatore@gmail.com>
2025-03-14 03:07:53 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,14 +13,20 @@ sudo systemctl start wg-quick@wg1
 sudo systemctl status wg-quick@wg1
 
 
-echo "Install Chrome Remote Desktop"
-cd /tmp
-sudo apt update
-sudo apt install -y wget 
+# echo "Install Chrome Remote Desktop"
+# cd /tmp
+# sudo apt update
+# sudo apt install -y wget 
 
-wget -O /tmp/chrome-remote-desktop_current_amd64.deb   https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo apt install -y /tmp/chrome-remote-desktop_current_amd64.deb
-sudo apt install -f
+# wget -O /tmp/chrome-remote-desktop_current_amd64.deb   https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+# sudo apt install -y /tmp/chrome-remote-desktop_current_amd64.deb
+# sudo apt install -f
 
-echo "Run Chrome Remote Desktop"
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AQSTgQHBTZMISFT0Q1Cc51VjnMWmRrt2b9KfpHwc83i1dpbO-iR1dCbS22IkvwElY22O-A" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+# echo "Run Chrome Remote Desktop"
+# DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="..." --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+
+echo "Install Teamviewer"
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo apt install -y -f ./teamviewer_amd64.deb
+echo "Start Teamviewer"
+teamviewer
